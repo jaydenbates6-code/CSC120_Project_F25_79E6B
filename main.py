cc9e44f360c49153b72b5d0a4970d37b2d694500
Your Name <jaydenbates6@gmail.com>
2025-10-03 19:49:34 -0400

    commit

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import random
 game_name = "Soyed's Quest"
-print("Welcome to " + game_name + "!")
-print("=========================")
+print(f"Welcome to {game_name}!")
+print("=" * 25)
 name = input("What is your name, adventurer? ")
-print("Great " + name+ "! Let's begin your quest!")
+print(f"Great {name}! Let's begin your quest!")
+player = {
+    "name": name,
+    "health": 100,
+    "coin" : 0
+}
+print(f"{player['name']}, you start with {player['health']} health and {player['coin']} coins.")
+
+def get_random_event():
+    events = ["find a coin", "encounter monster", "nothing"]
+    return random.choice(events)
+def update_stats(player, event):
+    if event == "find a coin":
+        player["coin"] += 1
+        print(f"{player['name']} found a coin, now has {player['coin']} coins.")
+    elif event == "encounter monster":
+        player["health"] -= 1
+        print(f"{player['name']} got hurt during the combat with a monster, health is now {player['health']}.")
+    else:
+        print(f"{player['name']} wandered around... nothing happened.") 
+for _ in range(3):
+    event = get_random_event()
+    print(f"While exploreing, you found: {event}")
+    update_stats(player,event)
